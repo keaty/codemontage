05f180c54da867ae7d7ada50d5999d35bef5b525
Keaty <holla@keaty.com>
2014-06-18 23:08:50 -0400

    Connect selected radio button to projects displayed
     * declare variable to hold filteredProjectIds in an array
     * determine whether project id matches id in filteredProjectIds
     * addClass 'hidden' to projects without selectedLanguage

--- app/assets/javascripts/projects.js
+++ app/assets/javascripts/projects.js
@@ -2,16 +2,25 @@ $(document).ready(function () {
 
   $("input:radio").click(function() {
 
-    var selected = $(this).val();
+    var selectedLanguage = $(this).val();
+    var filteredProjectIds = [];
+
+    for( var i = 0; i < projects_json.length; i++) {
+      var hasLanguage = false;
+
+      for( var j = 0; j < projects_json[i].technologies.length; j++) {
+        if( projects_json[i].technologies[j].name === selectedLanguage ) { hasLanguage = true };
+      };
+
+      if (hasLanguage === true || selectedLanguage === "All") { filteredProjectIds.push(projects_json[i].id) };
+    };
 
     $( "div.projects" ).children( ".row" ).each( function() {
-
-      if($(this).data('id') === selected) {
+      if (filteredProjectIds.indexOf($(this).data('id')) !== -1) {
         $( this ).removeClass('hidden');
       } else {
         $( this ).addClass('hidden');
       };
-
     });
 
   });
